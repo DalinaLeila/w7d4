e8b775422d22e3a1ddd990329d9f337e50ca0acd
Lukas Gisder-Dubé <gisderdube@gmail.com>
2018-09-13 15:27:25 +0200

    added clear name country borders

--- src/js/Countries/Details.jsx
+++ src/js/Countries/Details.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const Details = props => {
     const matchingCountry = props.countries.find(el => el.cca3 === props.match.params.cc)
-    console.log('matchingCountry', matchingCountry)
+
+    const mappedBorders = matchingCountry.borders.map(el => {
+        const foundCountry = props.countries.find(c => c.cca3 === el)
+        return (
+            <li>
+                <Link to={`/countries/${el}`}>{foundCountry.name.common}</Link>
+            </li>
+        )
+    })
 
     return (
         <div className="details">
@@ -12,6 +21,9 @@ const Details = props => {
             <h4>Capital: {matchingCountry.capital[0]}</h4>
             <hr />
             <h4>Area: {matchingCountry.area} km2</h4>
+            <hr />
+            <h4>Borders:</h4>
+            <ul>{mappedBorders}</ul>
         </div>
     )
 }
